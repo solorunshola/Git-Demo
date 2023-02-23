245c2093a7f0fb382d65f33d3aece9c5546b8e62
solorunishola <BYTEWORKS@BYTEWORKS>
2023-02-23 12:17:13 +0100

    Add test case to Git Demo

---
+++ Object Repository/Page_NIMC  CBS/a_Apply For Cetificate of Identity.rs
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Apply For Cetificate of Identity</name>
+   <tag></tag>
+   <elementGuidId>cc703088-462f-400a-89dd-58f24800b56b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[contains(text(),'Apply For Cetificate of Identity')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.btn.btn-outline-light.p-3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>116e99bd-9a7d-4951-939b-7e15a59dbd41</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-outline-light p-3</value>
+      <webElementGuid>76ff6e19-3cac-4113-8fb7-254b745797e0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/id-cert/instructions</value>
+      <webElementGuid>0e15aa4b-10d7-473f-8798-1eb2dc4f67bf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> Apply For Cetificate of Identity </value>
+      <webElementGuid>973cba89-bfa2-4201-bffa-3c79c94cc4de</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[@class=&quot;ng-tns-c191-0&quot;]/div[@class=&quot;ng-tns-c191-0&quot;]/div[@class=&quot;my-router-outlet ng-tns-c191-0&quot;]/app-home[@class=&quot;ng-star-inserted&quot;]/section[1]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;header-slide-bg w-100&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-6 p-md-5 py-5&quot;]/div[@class=&quot;p-md-5 p-4 header-info-card&quot;]/div[@class=&quot;mt-md-5 my-4&quot;]/a[@class=&quot;btn btn-outline-light p-3&quot;]</value>
+      <webElementGuid>973c915b-8629-4b79-b831-b2865c05b059</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Apply For Cetificate of Identity')]</value>
+      <webElementGuid>f34537b8-b947-4b1d-ad7f-54559c619bad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='CERTIFICATE OF IDENTITY'])[1]/following::a[1]</value>
+      <webElementGuid>bf8bf8f8-f3f9-4a44-803a-ff1e9fe34099</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Loading'])[1]/following::a[1]</value>
+      <webElementGuid>70ff4c08-4bb6-470c-ae80-11d1947f268d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Central Billing System Electronic Services'])[1]/preceding::a[1]</value>
+      <webElementGuid>08efe748-e3cd-4dd2-ae14-1bbcd1ba7c7d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='The CBS has Services to which third-party Apps and Clients can Integrate'])[1]/preceding::a[1]</value>
+      <webElementGuid>d8a0d9a9-2f55-4506-af37-62b420a6360d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Apply For Cetificate of Identity']/parent::*</value>
+      <webElementGuid>e835f43c-a423-4b11-b6e8-9372578b5e74</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/id-cert/instructions')]</value>
+      <webElementGuid>9efa9e89-947e-4016-abcc-5f29cb2d94c0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/a</value>
+      <webElementGuid>1587effe-27a5-41fc-8c81-5fc9264181c8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/id-cert/instructions' and (text() = ' Apply For Cetificate of Identity ' or . = ' Apply For Cetificate of Identity ')]</value>
+      <webElementGuid>00c1e04e-5d71-4148-a7a3-cc75c3083bc5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
